b7796bdd2e261b84443ddb67fb7c35196df2506f
Konstantin Kovshenin <kovshenin@gmail.com>
2021-12-02 11:18:59 +0300

    Add an uninstall.php routine
    Deletes wp_options entry, recursively deletes the cache directory,
    removes advanced-cache.php if its ours. Does NOT undefine
    WP_CACHE in wp-config.php, as there may be another caching plugin
    already active before somebody realizes to click the uninstall
    link in Surge. Play nice.

---
+++ uninstall.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Uninstall Surge
+ *
+ * @package Surge
+ */
+
+namespace Surge;
+
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	exit;
+}
+
+include_once( __DIR__ . '/include/common.php' );
+
+// Remove advanced-cache.php only if its ours.
+if ( file_exists( WP_CONTENT_DIR . '/advanced-cache.php' ) ) {
+	$contents = file_get_contents( WP_CONTENT_DIR . '/advanced-cache.php' );
+	if ( strpos( $contents, 'namespace Surge;' ) !== false ) {
+		unlink( WP_CONTENT_DIR . '/advanced-cache.php' );
+	}
+}
+
+// Delete the cache directory
+function delete( $path ) {
+	if ( is_file( $path ) ) {
+		unlink( $path );
+		error_log( 'unlink: ' . $path );
+		return;
+	}
+
+	if ( ! is_dir( $path ) ) {
+		return;
+	}
+
+	$entries = scandir( $path );
+	foreach ( $entries as $entry ) {
+		if ( $entry == '.' || $entry == '..' ) {
+			continue;
+		}
+
+		delete( $path . '/' . $entry );
+	}
+
+	rmdir( $path );
+	error_log( 'rmdir: ' . $path );
+}
+
+delete( CACHE_DIR );
+
+delete_option( 'surge_installed' );
