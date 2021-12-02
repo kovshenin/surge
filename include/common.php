125c416996a02eebc7b0b5bfcd8d977ffda87ccc
Konstantin Kovshenin <kovshenin@gmail.com>
2021-12-02 11:16:50 +0300

    Add more ignore_query_vars
    See: https://github.com/mpchadwick/tracking-query-params-registry/blob/master/_data/params.csv

--- include/common.php
+++ include/common.php
@@ -22,7 +22,23 @@ function config( $key ) {
 	return [
 		'ttl' => 600,
 		'ignore_cookies' => [ 'wordpress_test_cookie' ],
-		'ignore_query_vars' => [ 'utm_source', 'utm_medium', 'utm_campaign', 'utm_term', 'utm_content' ],
+
+		// https://github.com/mpchadwick/tracking-query-params-registry/blob/master/_data/params.csv
+		'ignore_query_vars' => [
+			'fbclid', 'gclid', 'gclsrc', 'utm_content', 'utm_term', 'utm_campaign',
+			'utm_medium', 'utm_source', 'utm_id', '_ga', 'mc_cid', 'mc_eid',
+			'_bta_tid', '_bta_c', 'trk_contact', 'trk_msg', 'trk_module', 'trk_sid',
+			'gdfms', 'gdftrk', 'gdffi', '_ke', 'redirect_log_mongo_id',
+			'redirect_mongo_id', 'sb_referer_host', 'mkwid', 'pcrid', 'ef_id',
+			's_kwcid', 'msclkid', 'dm_i', 'epik', 'pk_campaign', 'pk_kwd',
+			'pk_keyword', 'piwik_campaign', 'piwik_kwd', 'piwik_keyword', 'mtm_campaign',
+			'mtm_keyword', 'mtm_source', 'mtm_medium', 'mtm_content', 'mtm_cid',
+			'mtm_group', 'mtm_placement', 'matomo_campaign', 'matomo_keyword',
+			'matomo_source', 'matomo_medium', 'matomo_content', 'matomo_cid',
+			'matomo_group', 'matomo_placement', 'hsa_cam', 'hsa_grp', 'hsa_mt',
+			'hsa_src', 'hsa_ad', 'hsa_acc', 'hsa_net', 'hsa_kw', 'hsa_tgt',
+			'hsa_ver', '_branch_match_id',
+		],
 	][ $key ];
 }
 
